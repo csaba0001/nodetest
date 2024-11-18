68f3700200a799ef286af7c9675531e0cce8a0c6
bürgés csaba <c9burcsa@vasvari.org>
2024-11-18 09:21:07 +0100

    MŰKÖDIK AZ API POSTMANBAN 4!!!!!!!!!!!! MUGA
    also huge gitignore!!! shoutout  vsc gitignore extension

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 //    res.send('Hi, there!');
 //});
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
@@ -33,6 +33,66 @@ app.get("/nodejs", (req, res, next) => {
   res.send(
     "A Node.js egy olyan szerveroldali JavaScript futtatókörnyezet, amely a V8 JavaScript motorra épül."
   );
+});
+
+app.get("/api/users", (req, res) => {
+  res.status(200).json(users);
+});
+
+app.get("/api/users/:id", (req, res) => {
+  const userId = req.params.id;
+  const user = users.find((u) => u.id === userId);
+
+  if (!user) {
+    return res.status(404).json({ message: "User could not be found." });
+  }
+
+  res.status(200).json(user);
+});
+
+app.post("/api/users", (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ message: "Forgot to give name." });
+  }
+
+  const nUser = {
+    id: (users.length + 1).toString(),
+    name: name,
+  };
+
+  users.push(nUser);
+  res.status(201).json(nUser);
+});
+
+app.put("/api/users/:id", (req, res) => {
+  const userId = req.params.id;
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ message: "Forgot to give name." });
+  }
+
+  const userIndex = users.findIndex((u) => u.id === userId);
+  if (userIndex === -1) {
+    return res.status(404).json({ message: "Couldnt be foundd" });
+  }
+
+  users[userIndex].name = name;
+  res.status(200).json(users[userIndex]);
+});
+
+app.delete("/api/users/:id", (req, res) => {
+  const userId = req.params.id;
+  const userIndex = users.findIndex((u) => u.id === userId);
+
+  if (userIndex === -1) {
+    return res.status(404).json({ message: "Cant find" });
+  }
+
+  users.splice(userIndex, 1);
+  res.status(204).send();
 });
 
 app.listen(PORT, () => {
